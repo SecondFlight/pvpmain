45d57af1c6e2df646a002dfcc51c2bdd7f6b3f3d
SecondFlight <qbgeekjtw@gmail.com>
2014-07-08 21:56:38 -0400

    added more stuff, last commit before sleep

--- src/io/github/secondflight/abilities/LandMine.java
+++ src/io/github/secondflight/abilities/LandMine.java
@@ -1,8 +1,16 @@
 package io.github.secondflight.abilities;
 
+import java.awt.Event;
+
 import io.github.secondflight.util.DescriptionHandler;
 
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 public class LandMine implements Listener{
@@ -14,5 +22,19 @@ public class LandMine implements Listener{
 	
 	DescriptionHandler dh = new DescriptionHandler();
 	
+	@EventHandler
+	public void clickEvent (PlayerInteractEvent event) {
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			Player p = event.getPlayer();
+			ItemStack i = p.getItemInHand();
+			
+			if (dh.hasAggressiveAbility(i, "Land Mines")) {
+				newLandMine (p, event.getClickedBlock());
+			}
+		}
+	}
 	
+	public void newLandMine (Player p, Block b) {
+		
+	}
 }
